0447fd1f32c0cb09d3876bc5c975052480efedbe
mysoyul <vega2k_@naver.com>
2023-07-10 17:11:40 +0900

    HelloConfig add

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,24 +1,35 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
 import myspring.di.xml.Printer;
 import myspring.di.xml.StringPrinter;
 
 @Configuration
 @PropertySource("classpath:values.properties")
 public class HelloConfig {
+	@Autowired
+	private Environment env;
 	
 	@Bean
-	Printer strPrinter() {
+	public Hello hello() {
+		
+	}
+	
+	
+	@Bean
+	public Printer strPrinter() {
 		return new StringPrinter();
 	}
 	
 	@Bean
-	Printer conPrinter() {
+	public Printer conPrinter() {
 		return new ConsolePrinter();
 	}
 	
