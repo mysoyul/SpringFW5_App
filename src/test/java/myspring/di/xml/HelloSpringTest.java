ba060ff6ec20eb4c2e6820e071deb2481c6a4661
mysoyul <vega2k_@naver.com>
2023-07-10 14:25:17 +0900

    spring Test add

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -12,10 +13,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class HelloSpringTest {
 	@Autowired
+	@Qualifier("helloC")
 	Hello hello;
 
 	@Test
 	public void helloBean() {
 		System.out.println(hello.sayHello());
+		assertEquals("Hello 생성자", hello.sayHello());
 	}
 }
