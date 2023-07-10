1478db2f034a9ca3a2b1514e08aef20441f4d0e3
mysoyul <vega2k_@naver.com>
2023-07-10 14:38:36 +0900

    spring Test update

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,11 +17,15 @@ public class HelloSpringTest {
 	@Autowired
 	@Qualifier("helloC")
 	Hello hello;
-
+	
+	@Resource(name = "hello")
+	Hello myhello;
+	
 	@Test
 	public void helloBean() {
 		System.out.println(hello.sayHello());
-		assertEquals("Hello 생성자", hello.sayHello());
-		hello.print();
+		System.out.println(myhello.sayHello());
+		//assertEquals("Hello 스프링", myhello.sayHello());
+		myhello.print();
 	}
 }
