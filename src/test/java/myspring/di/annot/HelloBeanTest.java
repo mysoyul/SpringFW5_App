08132d095b19f2f9995a9073069b0a3e2c5f1914
mysoyul <vega2k_@naver.com>
2023-07-10 15:33:40 +0900

    AnnotTest update

--- src/test/java/myspring/di/annot/HelloBeanTest.java
+++ src/test/java/myspring/di/annot/HelloBeanTest.java
@@ -4,17 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration("classpath:spring-beansx.ml")
+@ContextConfiguration("classpath:spring-beans.xml")
 class HelloBeanTest {
-
+	@Autowired
+	HelloBean hello;
+	
+	@Autowired
+	HelloBean hello2;	
+	
+	@Autowired
+	@Qualifier("stringPrinter")
+	PrinterBean printer;
 	
 	@Test
 	void helloBean() {
-		
+		System.out.println(hello.sayHello());
+		assertEquals(hello.sayHello(), "Hello 어노테이션");
+		assertSame(hello, hello2);
 	}
 
 }
