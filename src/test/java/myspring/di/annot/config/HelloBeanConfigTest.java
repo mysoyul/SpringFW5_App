bad597583038ef7cea611e69f938ad5024c6a814
mysoyul <vega2k_@naver.com>
2023-07-10 16:46:27 +0900

    HelloBeanConfigTest add

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -1,11 +1,21 @@
 package myspring.di.annot.config;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration
-public class HelloBeanConfigTest {
+import myspring.di.annot.HelloBean;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HelloBeanConfig.class)
+public class HelloBeanConfigTest {
+	@Autowired
+	HelloBean hello;
+	
+	@Test
+	void helloBean() {
+		System.out.println(hello.sayHello());
+	}
 }
