bfaccf83e9cd7dba90fbb9dfdc23556a12007e65
mysoyul <vega2k_@naver.com>
2023-07-10 14:16:06 +0900

    spring Test add

---
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -0,0 +1,9 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class HelloSpringTest {
+
+}
