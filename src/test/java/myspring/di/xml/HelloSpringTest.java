0f401698abb2ee8a423e1c63ef7ad958a145fa19
mysoyul <vega2k_@naver.com>
2023-07-10 14:21:41 +0900

    spring Test add

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,9 +1,21 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class HelloSpringTest {
+	@Autowired
+	Hello hello;
 
+	@Test
+	public void helloBean() {
+		System.out.println(hello.sayHello());
+	}
 }
