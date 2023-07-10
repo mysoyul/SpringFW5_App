7537419ba6b11952e05569dbf24b276cefbba35e
mysoyul <vega2k_@naver.com>
2023-07-10 14:49:56 +0900

    spring Test update

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,11 +1,9 @@
 package myspring.di.xml;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.*;
-
 import javax.annotation.Resource;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
