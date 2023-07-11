01f636b61392b10685596e14eedde1f28863a7ef
mysoyul <vega2k_@naver.com>
2023-07-11 14:32:08 +0900

    aop add

--- src/main/java/myspring/aop/xml/PerformanceTraceAdvice.java
+++ src/main/java/myspring/aop/xml/PerformanceTraceAdvice.java
@@ -1,13 +1,22 @@
 package myspring.aop.xml;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 
 public class PerformanceTraceAdvice {
 	public Object trace(ProceedingJoinPoint joinPoint) throws Throwable {
 		//타겟 메서드의 signature 정보
-		String signatureString = joinPoint.getSignature().toShortString();		
-		System.out.println(signatureString + " 시작");
+		String methodName = joinPoint.getSignature().getName();		
+		System.out.println(methodName + " 시작");
+		//타겟 메서드이 argument 정보
+		for(Object arg : joinPoint.getArgs()) {
+			System.out.println(methodName + " 아규먼트 = " + arg);
+		}
+		//타겟 객체의 클래스명
+		System.out.println("Target Class Name ="+ joinPoint.getTarget().getClass().getName());
+		
 		//타겟의 메서드가 호출되기 전의 시간 
-		long start = System.currentTimeMillis();		
+		long start = System.currentTimeMillis();	
+		
 		try {
 			//new Object[] {new String("dooly")}
 			//타겟의 메서드 호출
@@ -16,8 +25,9 @@ public class PerformanceTraceAdvice {
 		} finally {
 			//타겟의 메서드가 호출된 후의 시간
 			long finish = System.currentTimeMillis();
-			System.out.println(signatureString + " 종료");
-			System.out.println(signatureString + " 실행 시간 : " + 
+			
+			System.out.println(methodName + " 종료");
+			System.out.println(methodName + " 실행 시간 : " + 
 			   (finish - start) + " ms");
 		}
 	}
