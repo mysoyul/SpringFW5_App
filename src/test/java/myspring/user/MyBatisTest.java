f552e40cb49b30439a6e5793ed3d468a606e990a
mysoyul <vega2k_@naver.com>
2023-07-11 10:11:19 +0900

    mybatistest update

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -1,11 +1,21 @@
 package myspring.user;
 
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class MyBatisTest {
-
+	@Autowired
+	DataSource dataSource;
+	
+	@Test
+	public void connection() {
+		
+	}
 }
