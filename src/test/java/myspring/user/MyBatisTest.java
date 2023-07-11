a5423a1dec87b8895741e3790769e1940ab582c5
mysoyul <vega2k_@naver.com>
2023-07-11 11:12:45 +0900

    mybatistest update

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -6,17 +6,30 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import myspring.user.vo.UserVO;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class MyBatisTest {
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Test
+	void user_sql() {
+		UserVO user = 
+				sqlSession.selectOne("userNS.selectUserById", "dooly");
+		
+	}
 	
 	@Test
 	public void connection() {
