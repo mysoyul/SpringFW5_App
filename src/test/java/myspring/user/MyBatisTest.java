07d3ca1f70d93f61d9a6442f373ec9396dbcb996
mysoyul <vega2k_@naver.com>
2023-07-11 11:37:01 +0900

    dao service class add

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -7,12 +7,14 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import myspring.user.dao.mapper.UserMapper;
 import myspring.user.vo.UserVO;
 
 @ExtendWith(SpringExtension.class)
@@ -24,7 +26,17 @@ public class MyBatisTest {
 	@Autowired
 	SqlSession sqlSession;
 	
+	@Autowired 
+	UserMapper userMapper;
+	
 	@Test
+	void mapper() {
+		UserVO user = userMapper.selectUserById("dooly");
+		System.out.println(user);
+	}
+	
+	
+	@Test @Disabled
 	void user_sql() {
 		UserVO user = 
 				sqlSession.selectOne("userNS.selectUserById", "dooly");
